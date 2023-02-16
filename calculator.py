a1e654e0867c60de2f5388210adca92f37db606a
JHawi13 <Joherwi@gmail.com>
2023-02-16 08:54:37 +0300

    Finalsimple calculation module

--- calculator.py
+++ calculator.py
@@ -1,27 +1,35 @@
 #Lets go
 Equation=input("Enter the equation")
 def any_simple_calculation (Equation):
-    operators=['/','X','+','-']
     for i in range(len(Equation)):
-        for x in range(len(operators)):
-            if Equation[i]=='/':
-                operator=Equation.index('/')
-                num1=int(Equation[0:operator])
-                num2=int(Equation[operator+1:])
-                return print(num1/num2)
-            elif Equation[i]=='X':
-                operator=Equation.index('X')
-                num1=int(Equation[0:operator])
-                num2=int(Equation[operator+1:])
-                return print(num1*num2)
-            elif Equation[i]=='+':
-                operator=Equation.index('+')
-                num1=int(Equation[0:operator])
-                num2=int(Equation[operator+1:])
-                return print(num1+num2)
-            elif Equation[i]=='-':
-                operator=Equation.index('-')
-                num1=int(Equation[0:operator])
-                num2=int(Equation[operator+1:])
-                return print(num1-num2)
+        if Equation[i]=='/':
+            operator=Equation.index('/')
+            num1=int(Equation[0:operator])
+            num2=int(Equation[operator+1:])
+            return print(num1/num2)
+        elif Equation[i]=='X':
+            operator=Equation.index('X')
+            num1=int(Equation[0:operator])
+            num2=int(Equation[operator+1:])
+            return print(num1*num2)
+        elif Equation[i]=='x':
+            operator=Equation.index('x')
+            num1=int(Equation[0:operator])
+            num2=int(Equation[operator+1:])
+            return print(num1*num2)
+        elif Equation[i]=='*':
+            operator=Equation.index('*')
+            num1=int(Equation[0:operator])
+            num2=int(Equation[operator+1:])
+            return print(num1*num2)
+        elif Equation[i]=='+':
+            operator=Equation.index('+')
+            num1=int(Equation[0:operator])
+            num2=int(Equation[operator+1:])
+            return print(num1+num2)
+        elif Equation[i]=='-':
+            operator=Equation.index('-')
+            num1=int(Equation[0:operator])
+            num2=int(Equation[operator+1:])
+            return print(num1-num2)
 any_simple_calculation(Equation)
